45d3dc29e652ec9f9a57f7ac217bc31c64a8cec7
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-10-13 19:30:16 -0300

    fix ventana 10

--- src/functions_asm.s
+++ src/functions_asm.s
@@ -70,41 +70,28 @@ productoEscalar12:
 
 @ void filtroVentana10(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitudVectorIn)
 filtroVentana10:
-	push {r4-r12}
-	mov r3, r1
-	mov r4, r2
-	mov r5, #0
-	.fv10L1: @ n veces (inicio en 0 vectorOut)
-		strh r5, [r3], #2
-		subs r4, #1
+		push {r4-r7}
+		mov r4,#0
+		mov	r3,#0
+	.fv10L1:
+		ldrh r6,[r0],2
+		add	r3,r3,r6
+		add	r4,1
+		sdiv r5,r3,r4
+		strh r5,[r1],2
+		cmp	r4,#10
 		bne .fv10L1
-	mov r3, r0
-	mov r4, r1
-	mov r5, r2
-	mov r6, 10
-	mov r9, #2
-	mov r10,#1
-	.fv10L2: @ 10 veces
-		.fv10L3: @ acumulo vectorIn en vectorOut
-			ldrh r7, [r3], #2
-			udiv r8, r7, r6
-			ldrh r11, [r4]
-			add r12, r11, r8
-			strh r12, [r4], #2
-			subs r5, #1
-			bne .fv10L3
-		@ Preparo los vectores para una nueva acumulación
-		@ "muevo" vectorIn a la izquierda y limito en 1 la longitud
-		mov r3, r0
-		mov r4, r1
-		add r4, r9
-		add r9, #2
-		mov r5, r2
-		sub r5, r10
-		add r10, #1
-		subs r6, #1
-		bne .fv10L2
-	pop {r4-r12}
+	.fv10L2:
+		ldrh r6,[r0],-20
+		add	r3,r3,r6
+		ldr	r6,[r0],+22
+		sub	r3,r3,r6
+		add	r4,1
+		sdiv r5, r3, r7
+		strh r5,[r1],2
+		cmp	r4,r2
+		bne	.fv10L2
+	pop {r4-r7}
 	bx lr
 
 
